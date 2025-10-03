e127c9cccfa11753eb841ee8c9c81ee8527c39d3
leticiaa-santos <leticiaa.santos.ti@gmail.com>
2025-10-03 09:32:37 -0300

    implementando testes

--- front/src/__tests__/CadastroUsuario.test.jsx
+++ front/src/__tests__/CadastroUsuario.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent, waitFor} from '@testing-library/react';
-import { describe, expect } from 'vitest';
+import { beforeEach, describe, expect } from 'vitest';
 import { CadUsuario } from '../Paginas/CadUsuario';
 
 //render: renderiza a minha tela
@@ -9,15 +9,28 @@ import { CadUsuario } from '../Paginas/CadUsuario';
 
 describe("Cadastro de Usuário", () => {
 
-    it("A tela é exibida", () => {
-        render(<CadUsuario/>);
+    beforeEach(() => {
+        render(<CadUsuario />);
+    });
+    
+    it("A tela é exibida", () => {    
 
         const nomeInput = screen.getByLabelText(/Nome/i);
         const emailInput = screen.getAllByLabelText(/E-mail/i);
-        const botao = screen.getByRole("button", {name: /Cadastrar/i });
+        const botao = screen.getByRole("button", {name:/Cadastrar/i });
 
         expect(nomeInput).toBeTruthy();
         expect(emailInput).toBeTruthy();
         expect(botao).toBeTruthy();
+    });
+
+    it("Erro quando o campo estiver vazio", async () => {
+
+        fireEvent.click(screen.getByRole("button", {name:/Cadastrar/i }));
+
+        await waitFor(() => {
+            expect(screen.getByText("Insira ao menos 1 caractere")).toBeTruthy();
+            expect(screen.getByText("Insira seu email")).toBeTruthy();
+        });
     });
 })
